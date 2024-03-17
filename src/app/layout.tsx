bb1b5f4b95ace15734b1b55fd405ca631a71f753
Serhii Lakodei <serhii.lakodei@gmail.com>
2024-03-17 12:33:58 +0200

    google analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ import { SettingsDrawer, SettingsProvider } from 'src/components/settings';
 import { CheckoutProvider } from 'src/sections/checkout/context';
 
 import { AuthProvider } from 'src/auth/context/auth0';
+import Script from 'next/script';
 
 // ----------------------------------------------------------------------
 
@@ -76,6 +77,34 @@ export default function RootLayout({ children }: Props) {
             </SettingsProvider>
           </LocalizationProvider>
         </AuthProvider>
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "li1e78f26q");
+          `
+          }}
+        />
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NTD9SPVX');</script>
+          `
+          }}
+        />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=GTM-NTD9SPVX`}
+        />
       </body>
     </html>
   );
