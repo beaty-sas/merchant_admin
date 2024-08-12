755ea5b5f89251777f642f4194371d9d42e8bd88
SerhiiLakodei <>
2024-08-12 19:41:41 +0300

    update clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,7 +85,7 @@ export default function RootLayout({ children }: Props) {
             c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
           t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
           y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "li1e78f26q");
+          })(window, document, "clarity", "script", "nhfufgpe2f");
           `
           }}
         />
